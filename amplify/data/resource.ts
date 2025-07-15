e040994496fc8b55babc5b063522b261cda061a2
Derces <44539380+Derces@users.noreply.github.com>
2025-07-15 18:57:40 +0900

    main

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -38,7 +38,8 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'userPool',
+    // defaultAuthorizationMode: 'userPool',
+    defaultAuthorizationMode: 'apiKey',
     // API Key is used for a.allow.public() rules
     apiKeyAuthorizationMode: {
       expiresInDays: 30,
